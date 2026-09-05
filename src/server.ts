0f430529f47ccbf28b6f2dfa2e0f94486ca26b17
Suyog Sigdel <suyogsigdel000@gmail.com>
2026-09-05 21:10:33 +0545

    fix: correct Dockerfile casing

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
-// app.get("/path", (req,res) => {...})
-// app.post("/path", (req,res) => {...})
-
 import express from "express";
 import dotenv from "dotenv";
 import { getHealth } from "./controllers/healthController.js";
 import { connectDB } from "./config/db.js";
+import cors from "cors";
 
 dotenv.config();
 console.log("MongoURI is", process.env.MONGO_URI ? "SET" : "MISSING");
@@ -13,6 +11,7 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors());
 app.get("/", getHealth);
 
 app.listen(PORT, () => {
