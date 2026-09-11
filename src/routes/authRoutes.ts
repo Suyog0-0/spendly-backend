80c540548b120ecef20a6b39744ec8c7d1aba2b2
Suyog Sigdel <suyogsigdel000@gmail.com>
2026-09-11 12:39:51 +0545

    feat: add protected profile route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,17 @@
 // src/routes/authRoutes.ts
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/authController.js";
+import {
+  registerUser,
+  loginUser,
+  getProfile,
+} from "../controllers/authController.js";
+import { isAuthenticated } from "../middleware/isAuthenticated.js";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", isAuthenticated, getProfile);
 
 export default router;
+ 
