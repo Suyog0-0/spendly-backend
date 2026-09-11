3d088f4bc978a7c1d9d128d8f5b6827278006eac
Suyog Sigdel <suyogsigdel000@gmail.com>
2026-09-11 12:39:51 +0545

    feat: add authentication middleware

---
+++ src/middleware/isAuthenticated.ts
@@ -0,0 +1,25 @@
+// src/middleware/isAuthenticated.ts
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export const isAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    (req as any).userId = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
